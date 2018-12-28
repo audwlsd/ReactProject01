ae7c3615a6cde7134941ed279f4d7b489cdae97a
audwlsd <audwlsd7782@naver.com>
2018-12-28 16:32:46 +0900

    state 와 component 연결

--- App.js
+++ App.js
@@ -3,11 +3,19 @@ import {StyleSheet, Text, View} from 'react-native'
 import Timer from './component/timer'
 import reducer from './Redux/reducer'
 import { createStore } from 'redux'
-import { provide } from 'react-redux'
+import { Provider } from 'react-redux'
+
+// 저장소 불러오기, provider 가 해줌. provider 가 store 를 하위 컴포넌트로 뿌려주는 역할을 함.
+let store = createStore(reducer)
+
 
 export default class App extends React.Component {
     render() {
-        return <Timer />
+        return (
+            <Provider store = {store}>
+                <Timer />
+            </Provider>
+        )
     }
 }
 
