d9aa6fffa90c0c4c7db544a3fc63c4e3ae42e4fe
audwlsd <audwlsd7782@naver.com>
2018-12-28 15:59:55 +0900

    react-redux 설치 및 앱에 import 과정까지 완료

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {StyleSheet, Text, View} from 'react-native'
 import Timer from './component/timer'
+import reducer from './Redux/reducer'
+import { createStore } from 'redux'
+import { provide } from 'react-redux'
 
 export default class App extends React.Component {
     render() {
